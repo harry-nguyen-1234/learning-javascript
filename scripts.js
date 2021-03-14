274bda1c5d055cc30ba4ae6ab10b9dc62680d85b
Huy Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-14 10:52:14 +1000

    Implement fizzbuzz

--- scripts.js
+++ scripts.js
@@ -13,13 +13,27 @@ function capitalise(string) {
     return firstLetter + subString.slice(1);
 }
 
-// Return the last letter of the string.
 function lastLetter(string) {
     strLength = string.length;
     return string[strLength - 1];
 }
 
-console.log(add7(1));
-console.log(multiply(2,3));
-console.log(capitalise("TEST"));
-console.log(lastLetter("test"));
+// When a user inputs a number
+// Loop from 1 to the entered number
+// If the current number is divisible by 3 then print "Fizz"
+// If the current number is divisible by 5 then print "Buzz"
+// If the current number is divisible by 3 and 5 then print "FizzBuzz"
+// Otherwise print the current number
+function fizzBuzz() {
+    let endNum = parseInt(prompt("Enter the ending number: "));
+    let valueStrPairs = [[3, "Fizz"], [5, "Buzz"]];
+    for (let i = 1; i <= endNum; i++) {
+        let outputString = i.toString() + " "; 
+        for (let j = 0; j < valueStrPairs.length; j++) {
+            if (i % valueStrPairs[j][0] == 0) {
+                outputString += valueStrPairs[j][1];
+            }
+        }
+        console.log(outputString);
+    }
+}
