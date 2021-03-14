28631150920b7b61e5b532c4a0b4b257f2ab8353
Huy Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-14 10:05:10 +1000

    Practice using functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-alert("test");
+function add7(num) {
+    return num + 7;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Return the string with only first letter capitalised
+function capitalise(string) {
+    firstLetter = string[0].toUpperCase();
+    subString = string.toLowerCase();
+    return firstLetter + subString.slice(1);
+}
+
+// Return the last letter of the string.
+function lastLetter(string) {
+    strLength = string.length;
+    return string[strLength - 1];
+}
+
+console.log(add7(1));
+console.log(multiply(2,3));
+console.log(capitalise("TEST"));
+console.log(lastLetter("test"));
