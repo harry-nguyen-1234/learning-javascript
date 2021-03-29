11c27b8650c7ff2dda46754e7b5586504bfe00e6
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-30 07:21:26 +1000

    Add new script for working with JavaScript DOM manipulation

---
+++ scripts2.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "Text content goes here";
+
+container.appendChild(content);
